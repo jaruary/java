cb94b73643a25fa4217c1eb530c4ab603eaa8594
crazydais <davenolan1@gmail.com>
2013-08-13 13:27:00 +0100

    add new Addition class to lab01

--- Lab01_Classes/src/lab01_PKG/MainClass.java
+++ Lab01_Classes/src/lab01_PKG/MainClass.java
@@ -1,12 +1,28 @@
 package lab01_PKG;
 
+import javax.swing.*;
+
 public class MainClass 
 {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		//	Main Class Data
+		
+		String temp;
+		int a, b, answer;
+		
+		//	Main Program
 
+		temp = JOptionPane.showInputDialog(null, "Please enter the value of A");
+		a = Integer.parseInt(temp);
+		
+		temp = JOptionPane.showInputDialog(null, "Please enter the value of B");
+		b = Integer.parseInt(temp);
+		
+		Addition add = new Addition(a, b);
+		answer = add.getAdditionAnswer();
+		
+		JOptionPane.showMessageDialog(null, "The answer is: " + answer);
 	}
-
 }
